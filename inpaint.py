3d4b0f18dbcf80c6c7026c122fc0b81fba82f208
Vinh Khuc <knvinh@gmail.com>
2022-04-29 19:05:12 +0000

    Misc fix

--- inpaint.py
+++ inpaint.py
@@ -18,7 +18,7 @@ def inpaint(model_file: str, image_file: str, masking_file: str, image_size: int
 
     diffusion_model = GaussianDiffusion(
         denoise_model,
-        n_steps=10,
+        n_steps=200,
         image_size=image_size,
         channels=3,
         device=DEVICE
@@ -38,6 +38,7 @@ def inpaint(model_file: str, image_file: str, masking_file: str, image_size: int
             Lambda(lambda t: (t * 2) - 1)
         ])
     image = image_transform(Image.open(image_file))
+    image = image.to(DEVICE)
 
     mask_transform = Compose([
             Resize(image_size),
@@ -45,12 +46,22 @@ def inpaint(model_file: str, image_file: str, masking_file: str, image_size: int
             ToTensor(),
             Lambda(lambda t: 1 - t)
         ])
-    mask = mask_transform(Image.open(masking_file))
+    mask = Image.open(masking_file)
+    mask = mask_transform(mask)
+    mask = mask.to(DEVICE)
+
+    # Manual masking
+    mask = torch.ones_like(image)
+    mask[:, :, 40:] = 0    
+
+    # DEBUG ONLY
+    input_image = image * mask
+    utils.save_image(input_image, "inpaint-input.png")
 
     repaint_steps = 1  # DEBUG: no resampling
     output_image = diffusion_model.inpaint(image, mask, repaint_steps=repaint_steps, batch_size=7)
     output_image = (output_image + 1) * 0.5
-    utils.save_image(output_image, 'inpaint.png')
+    utils.save_image(output_image, 'inpaint-output.png')
 
 
 def main():
